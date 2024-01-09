63372f96c9af8b93374a193f7078df046b3f6776
AlphaBs <ksi123456ab@naver.com>
2024-01-09 23:00:20 +0900

    t

---
+++ src/Areas/Identity/Pages/Admin/Index.cshtml.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AlphabetUpdateServer.Areas.Identity.Pages.Admin;
+
+public class IndexModel : PageModel
+{
+    
+}
