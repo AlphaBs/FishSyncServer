594caf03678cfa2388aaffc38a2f9b7376857639
AlphaBs <ksi123456ab@naver.com>
2024-09-14 00:26:37 +0900

    feat: add ChecksumStorageBucketFileManager

---
+++ src/Controllers/api/Buckets/ChecksumStorageBucketFileManagerController.cs
@@ -0,0 +1,58 @@
+﻿using AlphabetUpdateServer.Models.ChecksumStorages;
+using AlphabetUpdateServer.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AlphabetUpdateServer.Controllers.Api.Buckets;
+
+[ApiController]
+[Route("api/buckets/checksum-storage-bucket/file-manager")]
+public class ChecksumStorageBucketFileManagerController : ControllerBase
+{
+    private readonly ApplicationDbContext _context;
+    private readonly ChecksumStorageService _storageService;
+
+    public ChecksumStorageBucketFileManagerController(
+        ApplicationDbContext context,
+        ChecksumStorageService storageService)
+    {
+        _context = context;
+        _storageService = storageService;
+    }
+
+    [HttpGet("{StorageId}")]
+    public async Task<ActionResult> OnGetAsync(string storageId)
+    {
+        var storage = await _storageService.GetStorage(storageId);
+        if (storage is null)
+        {
+            return NotFound();
+        }
+
+        var orphanChecksumSet = new List<string>();
+        var files = storage.GetAllFiles();
+        await foreach (var file in files)
+        {
+            orphanChecksumSet.Add(file.Metadata.Checksum);
+        }
+
+        var checksums = await _context.Buckets
+            .Include(bucket => bucket.Files)
+            .Where(bucket => bucket.ChecksumStorageId == storageId)
+            .SelectMany(bucket => bucket.Files)
+            .Select(file => file.Metadata.Checksum)
+            .Distinct()
+            .ToListAsync();
+
+        foreach (var checksum in checksums)
+        {
+            orphanChecksumSet.Remove(checksum);
+        }
+        
+        return Ok(new
+        {
+            usedChecksums = checksums,
+            orphanChecksums = orphanChecksumSet
+        });
+    }
+}
