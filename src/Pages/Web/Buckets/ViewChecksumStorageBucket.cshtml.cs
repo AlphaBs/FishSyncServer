3cfeb4261c8ea81694eda0dc1b0c7f892688566d
AlphaBs <ksi123456ab@naver.com>
2024-12-18 16:22:34 +0900

    feat: ViewChecksumStorageBucket 페이지 UI 개선, 현재 사용량 표시

--- src/Pages/Web/Buckets/ViewChecksumStorageBucket.cshtml.cs
+++ src/Pages/Web/Buckets/ViewChecksumStorageBucket.cshtml.cs
@@ -1,4 +1,5 @@
-﻿using AlphabetUpdateServer.Models.Buckets;
+﻿using System.ComponentModel;
+using AlphabetUpdateServer.Models.Buckets;
 using AlphabetUpdateServer.Services.Buckets;
 using AlphabetUpdateServer.Services.Users;
 using Microsoft.AspNetCore.Authorization;
@@ -23,20 +24,19 @@ public class ViewChecksumStorageBucketModel : PageModel
 
     [BindProperty]
     public string Id { get; set; } = default!;
-
-    [BindProperty]
     public IEnumerable<string> Owners { get; set; } = [];
-
-    [BindProperty]
     public BucketLimitations Limitations { get; set; } = default!;
-
-    [BindProperty]
+    [DisplayName("사용중인 버킷 용량")]
+    public long CurrentBucketSize { get; set; }
+    [DisplayName("가장 큰 파일의 용량")]
+    public long CurrentMaxFileSize { get; set; }
+    [DisplayName("저장된 파일 수")]
+    public int CurrentFileCount { get; set; }
+    [DisplayName("이번달 동기화 횟수")]
+    public int CurrentMonthlySyncCount { get; set; }
     public string StorageId { get; set; } = default!;
-
-    [BindProperty]
     public IEnumerable<BucketFile> Files { get; set; } = [];
-
-
+    
     public async Task<ActionResult> OnGetAsync(string id)
     {
         var bucket = await _bucketService.FindBucketById(id);
@@ -47,9 +47,17 @@ public class ViewChecksumStorageBucketModel : PageModel
 
         Id = id;
         Limitations = bucket.Limitations;
-        StorageId = await _bucketService.GetStorageId(id);
         Files = await bucket.GetFiles();
+        CurrentMonthlySyncCount = await _bucketService.GetMonthlySuccessfulSyncCount(id);
+        StorageId = await _bucketService.GetStorageId(id);
         Owners = await _bucketOwnerService.GetOwners(id);
+
+        foreach (var file in Files)
+        {
+            CurrentBucketSize += file.Metadata.Size;
+            CurrentMaxFileSize = Math.Max(CurrentMaxFileSize, file.Metadata.Size);
+            CurrentFileCount++;
+        }
         return Page();
     }
 }
